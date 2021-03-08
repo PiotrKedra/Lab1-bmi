d8a2f0eac7b5efec00158a0dd940378efe559bdf
Piotr Kedra <piotr.k9750@gmail.com>
2021-03-08 16:42:21 +0100

    espresso error test

---
+++ app/src/androidTest/java/pl/edu/pwr/lab1/i258287/BmiInputErrorTest.kt
@@ -0,0 +1,72 @@
+package pl.edu.pwr.lab1.i258287
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BmiInputErrorTest {
+
+    private lateinit var zeroString: String
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun leaveEmptyMassInput_clickCountBtn_displayEmptyErrorMessage() {
+        onView(withId(R.id.bmiBtn))
+            .perform(click())
+
+        onView(withId(R.id.massError))
+            .check(matches(withText(R.string.error_mass_empty)))
+    }
+
+    @Test
+    fun leaveEmptyHeightInput_clickCountBtn_displayEmptyErrorMessage() {
+        onView(withId(R.id.bmiBtn))
+            .perform(click())
+
+        onView(withId(R.id.heightError))
+            .check(matches(withText(R.string.error_height_empty)))
+    }
+
+    @Before
+    fun initValidString() {
+        zeroString = "0"
+    }
+
+    @Test
+    fun typeZeroMassString_clickCountBtn_displayZeroErrorMessage() {
+        onView(withId(R.id.massInput))
+            .perform(typeText(zeroString), closeSoftKeyboard())
+
+        onView(withId(R.id.bmiBtn))
+            .perform(click())
+
+        onView(withId(R.id.massError))
+            .check(matches(withText(R.string.error_mass_zero)))
+    }
+
+    @Test
+    fun typeZeroHeightString_clickCountBtn_displayZeroErrorMessage() {
+        onView(withId(R.id.heightInput))
+            .perform(typeText(zeroString), closeSoftKeyboard())
+
+        onView(withId(R.id.bmiBtn))
+            .perform(click())
+
+        onView(withId(R.id.heightError))
+            .check(matches(withText(R.string.error_height_zero)))
+    }
+}
